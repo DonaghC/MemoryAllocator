d1d3804ef8bb13c9c54393e77fa656c7dc38fcc3
Denis Casey <116331671@umail.ucc.ie>
2020-02-11 17:42:05 +0000

    Added performance tests for deallocation for FF

--- test/performance_tests.cpp
+++ test/performance_tests.cpp
@@ -444,3 +444,467 @@ TEST(Allocation, NoSpace_50000FreeBlocksTooSmall)
     std::cout << "Time: " << time/1000000 << "ms\n";
 }
 
+TEST(Deallocation, First_1000Times)
+{
+    std::array<std::uint8_t, 256> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    double time = 0;
+
+    int i=0;
+    while (i<1000)
+    {
+        auto b1 = ma.allocate(1);
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(b1);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+
+        i++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, First_10000Times)
+{
+    std::array<std::uint8_t, 256> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    double time = 0;
+
+    int i=0;
+    while (i<10000)
+    {
+        auto b1 = ma.allocate(1);
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(b1);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+
+        i++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, First_50000Times)
+{
+    std::array<std::uint8_t, 256> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    double time = 0;
+
+    int i=0;
+    while (i<50000)
+    {
+        auto b1 = ma.allocate(1);
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(b1);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+
+        i++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergePrev_1000Times)
+{
+    std::array<std::uint8_t, 1000*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 1000> allocs;
+
+    auto addr = ma.allocate(1);
+    allocs[0] = addr;
+    
+    int i1=0;
+    while (i1<999 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 999)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<1000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs[i2]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i2++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergePrev_10000Times)
+{
+    std::array<std::uint8_t, 10000*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 10000> allocs;
+
+    auto addr = ma.allocate(1);
+    allocs[0] = addr;
+    
+    int i1=0;
+    while (i1<9999 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 9999)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<10000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs[i2]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i2++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergePrev_50000Times)
+{
+    std::array<std::uint8_t, 50000*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 50000> allocs;
+
+    auto addr = ma.allocate(1);
+    allocs[0] = addr;
+    
+    int i1=0;
+    while (i1<49999 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 49999)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<50000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs[i2]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i2++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergeNext_1000Times)
+{
+    std::array<std::uint8_t, 1000*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 1000> allocs;
+
+    auto addr = ma.allocate(1);
+    allocs[0] = addr;
+    
+    int i1=0;
+    while (i1<999 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 999)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<1000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs[999-i2]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i2++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergeNext_10000Times)
+{
+    std::array<std::uint8_t, 10000*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 10000> allocs;
+
+    auto addr = ma.allocate(1);
+    allocs[0] = addr;
+    
+    int i1=0;
+    while (i1<9999 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 9999)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<10000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs[9999-i2]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i2++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergeNext_50000Times)
+{
+    std::array<std::uint8_t, 50000*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 50000> allocs;
+
+    auto addr = ma.allocate(1);
+    allocs[0] = addr;
+    
+    int i1=0;
+    while (i1<49999 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 49999)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<50000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs[49999-i2]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i2++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergePrevNext_1000Times)
+{
+    std::array<std::uint8_t, 2001*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 1001> allocs1;
+    std::array<void*, 1000> allocs2;
+
+    auto addr = ma.allocate(1);
+    allocs1[0] = addr;
+    
+    int i1=0;
+    while (i1<1000 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs2[i1] = addr;
+
+        addr = ma.allocate(1);
+        allocs1[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 1000)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<1000)
+    {
+        ma.deallocate(allocs1[i2]);
+        i2++;
+    }
+
+    int i3=0;
+    while (i3<1000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs2[i3]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i3++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergePrevNext_10000Times)
+{
+    std::array<std::uint8_t, 20001*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 10001> allocs1;
+    std::array<void*, 10000> allocs2;
+
+    auto addr = ma.allocate(1);
+    allocs1[0] = addr;
+    
+    int i1=0;
+    while (i1<10000 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs2[i1] = addr;
+
+        addr = ma.allocate(1);
+        allocs1[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 10000)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<10000)
+    {
+        ma.deallocate(allocs1[i2]);
+        i2++;
+    }
+
+    int i3=0;
+    while (i3<10000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs2[i3]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i3++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
+
+TEST(Deallocation, MergePrevNext_50000Times)
+{
+    std::array<std::uint8_t, 100001*(1+NODESIZE)> arr;
+    FirstFitMemoryAllocator ma(arr);
+
+    std::array<void*, 50001> allocs1;
+    std::array<void*, 50000> allocs2;
+
+    auto addr = ma.allocate(1);
+    allocs1[0] = addr;
+    
+    int i1=0;
+    while (i1<50000 && addr != nullptr)
+    {
+        addr = ma.allocate(1);
+        allocs2[i1] = addr;
+
+        addr = ma.allocate(1);
+        allocs1[i1+1] = addr;
+
+        i1++;
+    }
+    if (i1 < 50000)
+    {
+        std::cout << "nullptr at i1=" << i1 << "\n";
+    }
+
+    double time = 0;
+
+    int i2=0;
+    while (i2<50000)
+    {
+        ma.deallocate(allocs1[i2]);
+        i2++;
+    }
+
+    int i3=0;
+    while (i3<50000)
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        ma.deallocate(allocs2[i3]);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+        
+        i3++;
+    }
+
+    std::cout << "Time: " << time/1000000 << "ms\n";
+}
